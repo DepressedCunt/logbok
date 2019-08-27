1379d440effe0b4f46de6cdd14ee28cc1bde558b
callum.lee <generalbelial@gmail.com>
2019-08-27 11:07:20 +0200

    Made some changes in code added onto base structure of inital commit

--- src/logbook.java
+++ src/logbook.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class logbook {
     public static void main(String[] args) {
+        System.out.println("What do you want to do ?" + "\n" + "1. Show Journal" + "\n" + "2. Write in Journal");
+        Scanner input = new Scanner(System.in);
+        String answer = input.nextLine();
 
+        if (answer.equals("1")) {
+            System.out.println("test");
+        }
     }
 }
